9b7af4d3308da65e5f8cb6d873e4439b076d5a7d
Lousheng <47648914+Lousheng@users.noreply.github.com>
2019-02-26 22:52:08 -0500

    Added comments to each method

--- dictionary_builder.py
+++ dictionary_builder.py
@@ -1,7 +1,7 @@
 class DictionaryBuilder:
-    _key_list = {}
+    _key_list = {}	# Dictionary containing column names and their created dictionaries
     
-    def build(df):
+    def build(df):	# Builds a dictionary of the unique entries in each non-numerical column and an assigned integer
         for x in list(df):
             if df[x].dtype is np.dtype('O'):
                 unique = df[x].unique()
@@ -12,11 +12,8 @@ class DictionaryBuilder:
                     i += 1
                 _key_list[x] = keys
                 
-    def apply(df):
-        '''
-	Will eventually apply the dictionary to the dataframe and return the new dataframe
-	'''
+    def apply(df):	# Applies the dictionary to the dataframe, replacing any non-numerical entries with integers
 	pass
     
-    def get():
+    def get():		# Returns the master dictionary containing the created dictionaries
         return _key_list
