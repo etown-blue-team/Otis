3e004c68504f8667ba8ebb1e2c5f82eacfd1282f
Lousheng <loushengm@gmail.com>
2019-03-15 23:11:53 -0400

    updated test file and sample data to have a shorter name

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import dictionary_builder as db
 
-df = pd.read_csv("FL_insurance_sample.csv")
+df = pd.read_csv("FL.csv")
 x = db.DictionaryBuilder()
 keys = x.build(df)
 df = x.map(df,keys)
