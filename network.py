45d1ffce4a7a467fc83c48c42244105bc3cd1389
Kevin Carman <36905432+CarmanK@users.noreply.github.com>
2019-03-27 02:22:24 -0400

    MOM get the camera, my neural network learned!

--- network.py
+++ network.py
@@ -6,14 +6,16 @@ class Network:
         # TODO:check to make sure that indata/outdata match # of rows
         self.inData = inData
         self.outData = outData
-        self.trainedHidden = []
-        self.trainedOutput = []
+        self.trainedInputWeights = []
+        self.trainedHiddenWeights = []
+        self.trainedOutputWeights = []
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
     def sigmoid_der(self, x):
-        return self.sigmoid(x) * (1 - self.sigmoid(x))
+        # return self.sigmoid(x) * (1 - self.sigmoid(x))
+        return x * (1 - x)
 
     def softmax(self, x):
         expX = np.exp(x)
@@ -21,15 +23,15 @@ class Network:
 
     def train(self, epochs):
         nodesInput = len(self.inData[0]) # will likely have to more complex than this
-        nodesOutput = 1 # determine if multiple outputs or  before this and adjust accordingly
+        nodesOutput = len(self.outData[0]) # determine if multiple outputs or  before this and adjust accordingly
         nodesHidden = math.ceil((nodesInput + nodesOutput) / 2) # Mean of the I/O nodes rounded up
         hiddenLayers = 2 # determine how many hidden layers the user wants, default to 1
 
         # Total number of weights = nodesInput * nodesHidden + nodesOutput * nodesHidden + nodesHidden * hiddenLayers
-        np.random.seed(100) # temporary
+        # np.random.seed(100) # temporary
         # Returns random values between [0,1) to a given shape
         inputWeights = np.random.rand(nodesInput, nodesHidden) #2x2 each row is the input weights of the hidden layer
-        hiddenWeights = np.random.rand(hiddenLayers - 1, nodesHidden, nodesHidden) #2x2x2
+        hiddenWeights = np.random.rand(hiddenLayers - 1, nodesHidden, nodesHidden) #1x2x2
         outputWeights = np.random.rand(nodesHidden, nodesOutput) #2x1
         
         learningRate = 0.5
@@ -72,71 +74,42 @@ class Network:
             # print(outputLayerNodeValues)
 
             # ===[ Back Propagation ]===
-            errorOut = np.power((outputLayerNodeValues - self.outData), 2)
-            meanSquaredError = errorOut.sum() / len(self.inData) # Mean squared error cost function
-            print(meanSquaredError)
 
-            # Output -> Hidden
+            # Output
             outputError = self.outData - outputLayerNodeValues
             outputDelta = outputError * self.sigmoid_der(outputLayerNodeValues)
             
-            # Hidden -> Hidden
+            # Output -> Hidden
             hiddenError = np.dot(outputDelta, outputWeights.T)
             hiddenDelta = hiddenError * self.sigmoid_der(hiddenLayerNodeValues[0])
 
             # Hidden -> Input
-            inputError = np.dot(hiddenDelta, inputWeights.T)
+            inputError = np.dot(hiddenDelta, hiddenWeights[0].T)
             inputDelta = inputError * self.sigmoid_der(inputLayerNodeValues)
            
             # Calculate the adjustment to the weights
             inputAdj = np.dot(self.inData.T, inputDelta)
-            hiddenAdj = np.dot(hiddenLayerNodeValues[0].T, hiddenDelta)
-            outputAdj = np.dot(hiddenLayerNodeValues[-1].T, outputDelta)
+            hiddenAdj = np.dot(inputLayerNodeValues.T, hiddenDelta)
+            outputAdj = np.dot(hiddenLayerNodeValues[0].T, outputDelta)
+
+            # print(inputAdj)
+            # print(hiddenAdj)
+            # print(outputAdj)
 
             # Update the Weights
             inputWeights += inputAdj
             hiddenWeights[0] += hiddenAdj
             outputWeights += outputAdj
 
-            # clays code
-            # hiddenWeights = np.random.rand(len(self.inData[0]),4) 
-            # outputWeights = np.random.rand(4,1)
-            #Create values for the Hidden Layer
-    #         hiddenWeightSum = np.dot(self.inData,hiddenWeights)
-    #         hiddenFinalNodeValue = self.sigmoid(hiddenWeightSum)
-
-    #         #Create values for the Output Layer
-    #         outputWeightSum = np.dot(hiddenFinalNodeValue,outputWeights)
-    #         outputFinalNodeValue = self.sigmoid(outputWeightSum)
-
-    #         # ===[ Back Propigation ]===
-
-    #         meanSquaredError = ((1/2) * (np.power((outputFinalNodeValue - self.inData), 2)))
-    #         print("[" + str(epoch) + "]Error %: " + str(meanSquaredError.sum))
-
-    #         #Update Output Weights
-    #         outPredObserved = outputFinalNodeValue - self.outData #Difference between predicted data and actual data a.k.a the error
-    #         outPred = self.sigmoid_der(outputWeightSum)
-
-    #         outputBackProp = np.dot(hiddenFinalNodeValue.T,outPredObserved * outPred)
-
-    #         #Update Hidden Weights
-    #         hiddenVal = outPredObserved * outPred
-            
-    #         hiddenCost = np.dot(hiddenVal, outputWeights.T)
-    #         hiddenBackProp = np.dot(self.outData.T,self.sigmoid_der(hiddenWeightSum) * hiddenCost)
-
-    #         #Update Weights
-    #         outputWeights -= learningRate * outputBackProp
-    #         hiddenWeights -= learningRate * hiddenBackProp
+        # Store the weights
+        self.trainedInputWeights = inputWeights
+        self.trainedHiddenWeights = hiddenWeights
+        self.trainedOutputWeights = outputWeights
 
 
-    #     #Store
-    #     self.trainedHidden = hiddenWeights
-    #     self.trainedOutput = outputWeights 
+    def run(self, data):
+        firstLayer = self.sigmoid(np.dot(data, self.trainedInputWeights))
+        hiddenLayer = self.sigmoid(np.dot(firstLayer, self.trainedHiddenWeights[0]))
+        outputLayer = self.sigmoid(np.dot(hiddenLayer, self.trainedOutputWeights))
 
-    # def run(self, data):
-    #     hiddenrun = self.sigmoid(np.dot(data,self.trainedHidden))
-    #     output = self.sigmoid(np.dot(hiddenrun,self.trainedOutput))
-
-    #     print(output)
+        print(outputLayer)
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import numpy as np
 from network import *
 
-data = np.array([[0,0],[0,1],[1,0],[1,1]])
-output = np.array([[0],[1],[1],[0]])
+# data = np.array([[0,0],[0,1],[1,0],[1,1]])
+# output = np.array([[0],[1],[1],[0]])
+# output = np.array([[0],[1],[1],[1]])
 
-neural = Network(data,output)
-Network.train(neural,1)
-# Network.run(neural,[0,0])
+data = np.array([[0,0,1],[0,1,1],[1,0,1],[0,1,0],[1,0,0],[1,1,1],[0,0,0]])
+output = np.array([[0],[1],[1],[1],[1],[0],[0]])
+
+
+
+neural = Network(data, output)
+Network.train(neural, 10000)
+Network.run(neural,[1,1,0])
 
